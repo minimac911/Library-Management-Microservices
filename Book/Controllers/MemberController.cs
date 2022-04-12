45912e534477941767c521e5a1aca290c1aaa3ce
minimac911 <thomasmcalpine77@gmail.com>
2022-04-12 21:26:05 +0200

    Created basic microservices

---
+++ Book/Controllers/MemberController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Book.Controllers
+{
+    [ApiController]
+    [Route("member")]
+    public class MemberController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

f8814fde68602abdf5aaeecb8ba5602593d203e1
Thomas <thomasmcalpine77@gmail.com>
2022-04-26 20:16:52 +0200

    Setup all services in ocelot

---
+++ sql/init.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS `services_computing_review`;
+CREATE DATABASE IF NOT EXISTS `services_computing_order`;
+CREATE DATABASE IF NOT EXISTS `services_computing_member`;
+CREATE DATABASE IF NOT EXISTS `services_computing_book`;
